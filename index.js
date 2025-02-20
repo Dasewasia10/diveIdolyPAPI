f116ee5c44edf7cf4b857ff37f78dacd85df1952
Dasewasia <77565642+Dasewasia10@users.noreply.github.com>
2025-02-20 21:46:54 +0800

    Fix index.js, change assert to with

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Contoh data
-import cardSources from "./src/data/card/cardSources.json" assert { type: "json" };
-import qnaSources from "./src/data/qna/qnaSources.json" assert { type: "json" };
-import lyricSources from "./src/data/lyrics/lyricsData.json" assert { type: "json" };
-import characterSources from "./src/data/character/character.json" assert { type: "json" };
+import cardSources from "./src/data/card/cardSources.json" with { type: "json" };
+import qnaSources from "./src/data/qna/qnaSources.json" with { type: "json" };
+import lyricSources from "./src/data/lyrics/lyricsData.json" with { type: "json" };
+import characterSources from "./src/data/character/character.json" with { type: "json" };
 
 // Middleware untuk parsing JSON
 app.use(json());
