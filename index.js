66025d467983336b06c31f279373932ccc92a22b
Dasewasia <77565642+Dasewasia10@users.noreply.github.com>
2025-02-24 06:50:32 +0800

    fix stamp in chat endpoint

--- index.js
+++ index.js
@@ -139,17 +139,11 @@ app.get("/api/stamps/:name", (req, res) => {
 });
 
 // Endpoint untuk gambar stamp
-app.get("/api/img/stamp/:imageCharacter/:imageExpression", async (req, res) => {
-  const { imageCharacter, imageExpression } = req.params;
+app.get("/api/img/stamps/:imageCharacter/:imageExpression", (req, res) => {
+  const { imageCharacter } = req.params;
+  const { imageExpression } = req.params;
   const imageUrl = `https://api.diveidolypapi.my.id/stampChat/stamp_${imageCharacter}-${imageExpression}.webp`;
-
-  try {
-    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    res.set('Content-Type', response.headers['content-type']);
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send('Error fetching image');
-  }
+  res.redirect(301, imageUrl); // 301: Permanent Redirect
 });
 
 // Mendapatkan data gambar icon character
